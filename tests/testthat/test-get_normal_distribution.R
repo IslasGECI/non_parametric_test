a66c911fc2b94a0f6c11ef5ef70a037a87f942ba
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 09:57:42 -0700

    🐛✔️ Fix test

--- tests/testthat/test-get_normal_distribution.R
+++ tests/testthat/test-get_normal_distribution.R
@@ -1,6 +1,6 @@
 describe("We can get normal distribution from a data", {
   data <- rnorm(100, 3, 2)
   new_data <- get_normal_distribution(data)
-  p_value <- ks.test(data, new_data)$p_value
+  p_value <- ks.test(data, new_data)$p.value
   expect_true(p_value > 0.05)
 })
