1ca01bd3f1859fd5ea6546bddbc7296278e41aff
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 12:31:49 -0700

    ✔️ Test first dataloger data

--- tests/testthat/test-real_examples.R
+++ tests/testthat/test-real_examples.R
@@ -3,5 +3,6 @@ library(tidyverse)
 describe("Are the data of humidity normal", {
   it("TS1_N", {
     humedity <- read_csv("/workdir/tests/data/Humedad_naturales.csv", show_col_types = FALSE)
+    expect_false(did_come_from_a_normal_distribution(humedity$TS1_N))
   })
 })
