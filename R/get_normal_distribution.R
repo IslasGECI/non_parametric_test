9d0641e0aacff03f40c18414f1fcb0c6d65af3b5
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 11:14:45 -0700

    ✔️ Pass test

--- R/get_normal_distribution.R
+++ R/get_normal_distribution.R
@@ -1,9 +1,10 @@
 get_normal_distribution <- function(data) {
   m <- mean(data)
   s <- sd(data)
-  return(rnorm(100, m, s))
+  return(rnorm(1000, m, s))
 }
 
 did_come_from_a_normal_distribution <- function(data) {
-  return(TRUE)
+  kst <- shapiro.test(data)
+  return(kst$p.value > 0.05)
 }
