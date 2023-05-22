8a44087d3bc321a60975a7ff6947226ebee9170b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 10:10:07 -0700

    🎨 Format with Styler

--- tests/testthat/test-get_normal_distribution.R
+++ tests/testthat/test-get_normal_distribution.R
@@ -1,14 +1,14 @@
 describe("We can get normal distribution from a data", {
   it("First example", {
-  data <- rnorm(100, 3, 2)
-  new_data <- get_normal_distribution(data)
-  p_value <- ks.test(data, new_data)$p.value
-  expect_true(p_value > 0.05)
-})
+    data <- rnorm(100, 3, 2)
+    new_data <- get_normal_distribution(data)
+    p_value <- ks.test(data, new_data)$p.value
+    expect_true(p_value > 0.05)
+  })
   it("Second example", {
-  data <- rnorm(100, 5, 1)
-  new_data <- get_normal_distribution(data)
-  p_value <- ks.test(data, new_data)$p.value
-  expect_true(p_value > 0.05)
-})
+    data <- rnorm(100, 5, 1)
+    new_data <- get_normal_distribution(data)
+    p_value <- ks.test(data, new_data)$p.value
+    expect_true(p_value > 0.05)
+  })
 })
