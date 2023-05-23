59b5dbc1a0c1691f073bc6ed645ed3edf7d68466
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 18:53:17 -0700

    🛑🧪 Test `select_petrel_valley()`: Third test

--- tests/testthat/test-real_examples.R
+++ tests/testthat/test-real_examples.R
@@ -20,4 +20,13 @@ describe("select_petrel_valley", {
     expected_names <- c("TS12_N", "TS13_N")
     expect_equal(obtained_names, expected_names)
   })
+  it("Naturals and artificials", {
+    humedity <- read_csv("/workdir/tests/data/Humedad.csv", show_col_types = FALSE) |>
+      drop_na()
+    petrel_valley <- humedity |>
+      select_petrel_valley()
+    obtained_names <- names(petrel_valley)
+    expected_names <- c("TS10_A", "TS11_A", "TS12_N", "TS13_N")
+    expect_equal(obtained_names, expected_names)
+  })
 })
