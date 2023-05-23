edf999dc0a3a90abca8adb8b4838b1830ca160e6
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-22 18:17:54 -0700

    ✅🗃️ Add test of artificial burrows

--- tests/testthat/test-real_examples.R
+++ tests/testthat/test-real_examples.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 describe("Are the data of humidity normal", {
-  it("TS1_N", {
-    humedity <- read_csv("/workdir/tests/data/Humedad_naturales.csv", show_col_types = FALSE) |>
+  it("All datalogers", {
+    humedity <- read_csv("/workdir/tests/data/Humedad.csv", show_col_types = FALSE) |>
       drop_na()
     for (i in 2:ncol(humedity)) {
       expect_false(did_come_from_a_normal_distribution(humedity[[i]]))
